12e16ffac72c7b35db7f7fd6df8fcec5ca128e4d
AlinCervantes <zenikninq1822@gmail.com>
2025-12-08 09:22:12 -0600

    PROYECTO COMPLETADO - VERSIÓN 1.0

--- generales/views.py
+++ generales/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from catalogos.models import Vehiculo, Propietario, Oficina
+from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    return render (
-        request,
-        'home.html'
-    )
+    # Obtener conteos para mostrar en home
+    total_vehiculos = Vehiculo.objects.count()
+    total_propietarios = Propietario.objects.count()
+    total_oficinas = Oficina.objects.filter(activo=True).count()
+    
+    return render(request, 'home.html', {
+        'total_vehiculos': total_vehiculos,
+        'total_propietarios': total_propietarios,
+        'total_oficinas': total_oficinas,
+        'total_activos': '100%',  # Puedes cambiar esto si tienes lógica de status
+    })
     
